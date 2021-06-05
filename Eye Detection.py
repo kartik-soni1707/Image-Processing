c7fba618c1d8022635620829f7ae9678257b592b
kartik-soni1707 <kartik.soni1404@gmail.com>
2021-06-05 13:44:46 +0530

    Eye detector

--- Eye Detection.py
+++ Eye Detection.py
@@ -1,14 +1,21 @@
 import cv2 as cv
 face_class=cv.CascadeClassifier('hc.xml')
+eye_class=cv.CascadeClassifier('hc1.xml')
 import numpy as np
 from matplotlib import pyplot as plt
 cap=cv.VideoCapture(0)
 while(cap.isOpened()):
     _,img=cap.read()
     gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    faces=face_class.detectMultiScale(img,1.1,4)
+    faces=face_class.detectMultiScale(gray,1.1,4)
+
     for (x,y,w,h) in faces:
         cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+        roi_b=gray[y:y+h,x:x+w]
+        roi = img[y:y + h, x:x + w]
+        eyes = eye_class.detectMultiScale(roi_b)
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 3)
     cv.imshow('frame',img)
     if cv.waitKey(1)==ord('q'):
         break
